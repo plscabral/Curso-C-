cd17d230fb31683a83cc0aff191eba8a3f587399
= <=>
2020-06-07 23:00:26 -0300

    Add classes-metodos-selados

---
+++ classes-metodos-selados/Entities/SavingsAccount.cs
@@ -0,0 +1,25 @@
+namespace aula.Entities
+{
+    public sealed class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount(){
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+        :base(number, holder, balance) {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance(){
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdrawn(double amount) {
+            base.Withdrawn(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ classes-metodos-selados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
