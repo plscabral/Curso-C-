f28744d9e1e964b557b4a5f8941d541984117e35
Paulo Luiz <paulo.luiz127@gmail.com>
2020-06-22 12:57:57 -0300

    Add HashSet-SortedSet-Hash-igualdade

---
+++ Hash-igualdade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using aula.Entities;
+
+namespace aula
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/* Quando usamos classe e estamos instanciando um objeto igualzinho e o programa sempre aponta 
+			 * como 'false' é porque a nossa classe não tem implementado o GetHashCode e o Equals. Agora quando
+			 * usamos um struct ele vai olhar diretamente para o conteudo do objeto e não para a referencia de memoria
+			 * a qual está sendo instânciada */
+
+			HashSet<Product> a = new HashSet<Product>();
+			a.Add(new Product("TV", 900.00));
+			a.Add(new Product("Notebook", 1200.00));
+
+			HashSet<Point> b = new HashSet<Point>();
+			b.Add(new Point(3, 4));
+			b.Add(new Point(5, 10));
+
+			Product prod = new Product("Notebook", 1200.00);
+			Console.WriteLine(a.Contains(prod));
+
+			Point p = new Point(5, 10);
+			Console.WriteLine(b.Contains(p));
+
+		}
+	}
+}
---
+++ HashSet-SortedSet-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<string> set = new HashSet<string>();
+
+			set.Add("Tv");
+			set.Add("Notebook");
+			set.Add("Tablet");
+
+			Console.WriteLine(set.Contains("Computer"));
+
+			foreach(string p in set)
+			{
+				Console.WriteLine(p);
+			}
+		}
+	}
+}
