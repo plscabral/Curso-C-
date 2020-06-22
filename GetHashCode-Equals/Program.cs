eb118c040b6eeea19189c56531a7de80b7ada8c1
Paulo Luiz <paulo.luiz127@gmail.com>
2020-06-22 11:00:40 -0300

    Add GetHashCode-Equals

---
+++ GetHashCode-Equals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using aula.Entities;
+
+namespace aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client{  Name = "Maria", Email = "maria@gmail.com"   };
+            Client b = new Client{  Name = "Alex", Email = "alex@gmail.com"   };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            /* Primeiro faz o GetHashCode pelo fato de ser mais rádipo e depois 
+            confimar usando o Equals, que é mais preciso! */
+        }
+    }
+}
---
+++ restricoes-generics/Entities/Product.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Globalization;
+
+namespace aula.Entities
+{
+     public class Product : IComparable
+     {
+          public string Name { get; set; }
+          public double Price { get; set; }
+
+          public Product(string name, double price)
+          {
+               Name = name;
+               Price = price;
+          }
+
+          public override string ToString()
+          {
+               return Name + ", " + Price.ToString("f2", CultureInfo.InvariantCulture);
+          }
+
+          public int CompareTo(object obj)
+          {
+               if (!(obj is Product))
+               {
+                    throw new ArgumentException("Comparing erro: argument is not a Product");
+               }
+               Product other = obj as Product;
+               return Price.CompareTo(other.Price);
+
+          }
+     }
+}
