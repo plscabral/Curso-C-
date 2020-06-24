185751603880ec51286ce08271f6bef202bababa
Paulo Luiz <paulo.luiz127@gmail.com>
2020-06-24 02:53:32 -0300

    Completo

---
+++ Linq-SQL-demo/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace aula.Entities
+{
+	class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public Category Category { get; set; }
+
+		public override string ToString()
+		{
+			return Id
+				+ ", "
+				+ Name
+				+ ", "
+				+ Price.ToString("f2", CultureInfo.InvariantCulture)
+				+ ", "
+				+ Category.Name
+				+ ", "
+				+ Category.Tier;
+		}
+	}
+}
---
+++ exercicios-curso/exercicio-fixação17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using aula.Entities;
+
+namespace aula
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter full file path: ");
+			string path = Console.ReadLine();
+
+			List<Product> products = new List<Product>();
+
+			using (StreamReader sr = File.OpenText(path))
+			{
+				while (!sr.EndOfStream)
+				{
+					string[] fields = sr.ReadLine().Split(',');
+					string name = fields[0];
+					double price = double.Parse(fields[1],CultureInfo.InvariantCulture);
+					products.Add(new Product(name, price));
+				}
+			}
+
+			var avg = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+			Console.WriteLine("Average price = " + avg.ToString("f2", CultureInfo.InvariantCulture));
+
+			var names = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+			foreach(string name in names)
+			{
+				Console.WriteLine(name);
+			}
+		}
+	}
+}
